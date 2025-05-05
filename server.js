0db817d957640bfec57c0f087db1df491147787e
MrGwin <mr.gvinblade@gmail.com>
2025-05-05 14:34:44 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.post('/api/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log('Received form data:', { name, email, message });
+  const responseMessage = `Спасибо за ваш интерес, ${name}`;
+  res.json({ message: responseMessage });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port:${port}`);
+});
